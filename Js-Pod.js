a28d6a2495f6a5296b28aa4139fd7b422333bce9
MrOffer <thomas.e.offer@gmail.com>
2019-10-21 16:47:26 -0500

    set up exercises

---
+++ Js-Pod.js
@@ -0,0 +1,79 @@
+// Write a function, adminList() that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+//
+//     Example Input:
+
+    [
+        {
+            isAdmin: true,
+            email: 'user1@email.com'
+        },
+        {
+            isAdmin: true,
+            email: 'user2@email.com'
+        },
+        {
+            isAdmin: false,
+            email: 'user3@email.com'
+        }
+    ];
+// Example Output (before refactor): 2
+//
+// Example Output (after 1st refactor):
+//
+// [
+//     'user1@email.com',
+//     'user2@email.com'
+// ]
+// Example Output (after 2nd refactor):
+//
+// [
+//     {
+//         isAdmin: true,
+//         email: 'user1@email.com'
+//     },
+//     {
+//         isAdmin: true,
+//         email: 'user2@email.com'
+//     }
+// ]
+
+
+// Create a function, makeSandwhichObjects() that takes in two array of strings, breads and fillings and returns an array of sandwhichObjects that contain properties for bread and filling and values correspond to the same order of the two passed in arrays. Assume the two array inputs are the same length.
+//
+//     Example Input:
+
+    var breads  = [
+        "white",
+        "wheat",
+        "rhy",
+        "white"
+    ];
+
+var fillings = [
+    "pb&j",
+    "ham",
+    "cheese steak",
+    "tuna"
+];
+
+// makeSandwhichObjects(breads, fillings)
+// Example Output:
+//
+//     [
+//         {
+//             bread: "white,
+//             filling: "pb&j"
+//         },
+//         {
+//             bread: "wheat",
+//             filling: "ham"
+//         },
+//         {
+//             bread: "rhy",
+//             filling: "cheese steak"
+//         },
+//         {
+//             bread: "white",
+//             filling: "tuna"
+//         }
+//     ]
