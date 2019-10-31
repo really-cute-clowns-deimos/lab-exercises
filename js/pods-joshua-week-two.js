f8729bbc85fb0b2f33517b04f6dceb4cbff64313
Joshua Peters <peters.l.joshua@gmail.com>
2019-10-31 11:43:11 -0500

    saving files for lab exercises

---
+++ js/pods-joshua-week-two.js
@@ -0,0 +1,62 @@
+// window.onload = function() {
+//     alert( 'The page has finished loading!' );
+// };
+
+
+$(document).ready(function() {
+    // alert( 'The DOM has finished loading!' );
+});
+
+$('.nav-wrapper').css('background-color', '#6EA4B3');
+
+$('.codeup').css('border','2px solid black');
+$('.codeup').css('box-shadow','1px 2px 4px 1px');
+
+// LI HOVER TO RED + CHANGE TEXT TO WHITE
+$('li').hover(
+    function() {
+        $(this).css('background-color', 'red').css('-webkit-transition', '1.5s').css('color', 'white');
+        // $(this).css('text-color', 'white').css('-webkit-transition', '2s');
+    },
+    function() {
+        $(this).css('background-color', '#6EA4B3').css('-webkit-transition', '1.5s').css('color', 'black');
+        // $(this).css('text-color', 'black').css('-webkit-transition', '2s');
+    }
+);
+// CLICK CLASS AND EXPAND TO CLASS TYPE
+$('#tank-list').hide();
+
+$('#tank-toggle').click(function() {
+    $('#tank-list').slideToggle();
+});
+
+$('li').css('font-size','20px');
+$('li').css('text-align','center');
+$('ul').css('list-style', 'none');
+
+
+$('h1').click(function() {
+    $(this).css('background-color', '#6EA4B3').css('-webkit-transition', '2s');
+
+});
+$('h1').css('font-size', '50px');
+$('h3').css('font-size', '30px');
+$('h5').css('font-size', '20px');
+$('h1').css('text-align', 'center');
+$('h3').css('text-align', 'center');
+$('h5').css('text-align', 'center');
+
+$('#class-bottom-div').css('background-color','#6EA4B3');
+$('#class-middle-div').css('background-color','#6EA4B3');
+
+
+$('#about-game').css('font-style','italic');
+
+$('p').dblclick(function(){
+    $(this).css('font-size', '18px').css('-webkit-transition', '2s');
+});
+$('p').css('font-size', '25px');
+$('p').css('text-align', 'center');
+
+$('h1, p, li').css('background-color', '');
+$('body').css('background-color', 'lightblue');
