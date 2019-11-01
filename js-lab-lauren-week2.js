2a6ed3a87eb7ff4c36f82a6f9d20f2b02e0a963b
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-10-31 22:24:14 -0500

    change h2 when click on the doggo image

--- js-lab-lauren-week2.js
+++ js-lab-lauren-week2.js
@@ -1,4 +1,5 @@
 "use strict";
+
 //create an array to hold piano
 var pianoSong = [80, 76, 65, 89];
 
@@ -11,7 +12,7 @@ $(document).keydown(function (event) {
 
     if (event.which === 80) {
         if (pianoSong[codeIndex] === 80) {
-            codeIndex++
+            codeIndex++;
             console.log("P");
         } else {
             codeIndex = 0;
@@ -59,21 +60,44 @@ function success() {
 
 };
 
+//when you click on the dog so that it spins
 $(function() {
-
-    var dog = $('#doggo')
+    //set doggo id of the element to var dog
+    var dog = $('#doggo');
+    var doggoHeader = $('h2');
 
     dog.click(function(){
+        //$this makes the element into a jquery object so that you can use jquery methods (like data)
         var doggoo = $(this);
+        //.data is used to attach data to, get info from selected elements
 
+
+        //checks if rotating is true, if it is then when you click it will stop
         if(doggoo.data('rotating')){
+            doggoHeader.text("Click the Doggo").css('font-family','Just Another Hand', 'cursive').css('font-size','40px').css('color', 'white');
+            //clearInterval clears the timer that was set when it first started spinning
             clearInterval(doggoo.data('rotating'));
+            //after clearing, set rotating(key) = false(value)
             doggoo.data('rotating', false)
-        } else {
+        }
+
+        //start spinning the image
+        else {
+
+            doggoHeader.text('Party Time!!').css('font-family', 'Faster One', 'cursive').css('font-size','60px').css('color', '#0F3CFF');
+            //setInterval is used as the parameter for the clearInterval
             doggoo.data('rotating', setInterval(function(){
-                var degree = doggoo.data('degree') || 0;
-                doggoo.css({ transform: 'rotate(' + degree + 'deg)'});
-                doggoo.data('degree', ++degree)
+
+                //set degreeIncrement to either the value of degree or zero
+                var degreeIncrement = doggoo.data('degreeKey') || 0;
+
+                //update css to transform the image (deg is degrees)
+                doggoo.css({ transform: 'rotate(' + degreeIncrement + 'deg)'});
+
+                //update the degreeKey's value by incrementing the degreeIncrement(value) variable
+                doggoo.data('degreeKey', ++degreeIncrement)
+
+                //the timeout creates a loop, causing the above code (the 'else') runs every 5 milliseconds
             }, 5));
         }
     });
@@ -82,3 +106,14 @@ $(function() {
 
 
 
+//for the buttons to show and hide the ipsums
+$(document).ready(function() {
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function() {
+        $("p").show();
+    })
+});
+
+$('p').css('font-family','Just Another Hand', 'cursive').css('font-size','40px');
