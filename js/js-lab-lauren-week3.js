86acf922a45b2cf9fbedbd6931fc633a24c070e5
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-04 12:17:18 -0600

    added some squares, another one that moves

--- js/js-lab-lauren-week3.js
+++ js/js-lab-lauren-week3.js
@@ -1,9 +1,10 @@
-$( "#run" ).click(function() {
+$( "#submit" ).click(function() {
     $( "div:animated" ).toggleClass( "colored" );
 });
 
+//mover is the box that is going to move
 function animateIt() {
-    $( "#mover" ).slideToggle( "slow", animateIt );
+    $( ".mover" ).slideToggle( "slow", animateIt );
 }
 
 animateIt();
