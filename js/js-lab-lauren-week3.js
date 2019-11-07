e3b4ea29e98790f14574e18dc7a7b09f4a135a8c
ltovar03 <55413265+laurentovar@users.noreply.github.com>
2019-11-07 11:07:03 -0600

    new square that moves when animate is clicked

--- js/js-lab-lauren-week3.js
+++ js/js-lab-lauren-week3.js
@@ -1,10 +1,24 @@
-$( "#submit" ).click(function() {
-    $( "div:animated" ).toggleClass( "colored" );
-});
+//make the boxes move up and down
+    $( "#submit" ).click(function() {
+        $( "div:animated" ).toggleClass( "colored" );
+    });
 
 //mover is the box that is going to move
-function animateIt() {
-    $( ".mover" ).slideToggle( "slow", animateIt );
-}
+    function animateIt() {
+        $( ".mover" ).slideToggle( "slow", animateIt );
+    }
 
-animateIt();
+    animateIt();
+
+//to make the single box animate
+$(document).ready(function () {
+    $('#animate').click(function () {
+        $('.box')
+            .animate({width: "200px"})
+            .animate({height: "200px"})
+            .animate({marginLeft: "160px"})
+            .animate({width: "200px"})
+            .animate({borderWidth: "10px"})
+            .animate({opacity: 0.5})
+    })
+});
